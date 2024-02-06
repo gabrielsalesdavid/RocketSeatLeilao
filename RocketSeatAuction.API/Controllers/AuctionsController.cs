972fe36479f0f066b07a1f63ffdebb3566011b4d
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-02-06 13:40:33 -0300

    Aula01

---
+++ RocketSeatAuction.API/Controllers/AuctionsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RocketSeatAuction.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AuctionsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCurrentAuction()
+    {
+        return Ok("Gabriel");
+    }
+    [HttpGet("test")]
+    public IActionResult Test()
+    {
+        return NotFound("Gabriel");
+    }
+}
